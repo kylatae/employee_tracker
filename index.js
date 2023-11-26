2f37f96334c3b2a18c00ed7d696434bf3884c6cf
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-11-26 14:05:35 -0600

    Changelog - Found typo's in schema.sql - Added Logo functionality that changes with menu option. - Added quit and view all employee functionality

--- index.js
+++ index.js
@@ -1,56 +1,115 @@
 const inquirer = require('inquirer');
-const db = require('db/db.js')
 
+const db = require('./db/db.js')
 
-qPrompt = [
-  {
-    type: 'list',
-    message: 'What would you like to do? (Use Arrow Keys)',
-    name: 'menuChoice',
-    choices: 
-      ['View All Employees', 
-      'Add Employee', 
-      'Update Employee Role',
-      'View All Roles',
-      'Add Role',
-      'View All Departments',
-      'Add Department',
-      'Quit'] 
-  }
-]
-
-
-
-function mainMenu() {
- 
-  inquirer.prompt(qPrompt)
-  .then((data=>{
-    switch(data) {
-      case 'View All Employees':
-        viewAllEmployees();
-        break;
-      case 'Add Employee':
-        addEmployee();
-        break;
-      case 'Update Employee Role':
-        updateEmployeeRole();
-        break;
-      case 'View All Roles':
-        viewAllRoles();
-        break;
-      case 'Add Role':
-        addRole();
-        break;
-      case 'View All Departments':
-        viewAllDepartments();
-        break;
-      case 'Add Department':
-        addDepartment();
-    }
-
-  }))
-
-  
+function programLogo(itemTitle){
+  console.clear()
+  console.log(`█████████████████████████████████████`)
+  console.log(`█                                   █`)
+  console.log(itemTitle)
+  console.log(`█                                   █`)
+  console.log(`█████████████████████████████████████`)
 }
 
+const mainMenu = () =>{
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: `
+What would you like to do?`,
+      name: 'menuChoice',
+      choices: 
+        ['View All Employees', 
+        'Add Employee', 
+        'Update Employee Role',
+        'View All Roles',
+        'Add Role',
+        'View All Departments',
+        'Add Department',
+        'Quit'] 
+    }
+  ]).then((data=>{
+      console.log("in the then")
+      switch(data.menuChoice) {
+        case 'View All Employees':
+          viewAllEmployees();
+          break;
+        case 'Add Employee':
+          addEmployee();
+          break;
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+        case 'View All Roles':
+          viewAllRoles();
+          break;
+        case 'Add Role':
+          addRole();
+          break;
+        case 'View All Departments':
+          viewAllDepartments();
+          break;
+        case 'Add Department':
+          addDepartment();
+          break;
+        case 'Quit':
+          exitProgram();
+      }
+  }))
+
+};
+
+
+const viewAllEmployees = () =>{
+  programLogo(`█         View All Employees        █`);
+  const req = `SELECT * FROM employee`;
+  db.promise().query(req)
+    .then((data)=>{
+      console.table(data[0]);
+      mainMenu();    
+    })
+    .catch((err) => console.log(err));
+}
+
+const addEmployee = () =>{
+
+
+  mainMenu();
+}
+
+const updateEmployeeRole = () =>{
+
+
+  mainMenu();
+}
+
+const viewAllRoles = () =>{
+
+
+  mainMenu();
+}
+
+const addRole = () =>{
+
+  mainMenu(); 
+}
+
+const viewAllDepartments = () =>{
+
+
+  mainMenu();
+}
+
+const addDepartment = () =>{
+
+
+  mainMenu();
+}
+
+const exitProgram = () =>{
+  programLogo(`█ Thanks for using Employee Tracker █`);
+  console.log ('')
+  process.exit(1)
+}
+programLogo(`█          Employee Tracker         █`);
 mainMenu();
